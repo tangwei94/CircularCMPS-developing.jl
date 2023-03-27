986ad575c06c6c7363e8751a4b3edb58569a4579
Wei Tang <wei.tang.nju@gmail.com>
2023-03-27 18:31:52 +0200

    update fig

--- scripts/lieb-liniger-spectra/spectrum-fixN.jl
+++ scripts/lieb-liniger-spectra/spectrum-fixN.jl
@@ -11,7 +11,6 @@ using CircularCMPS
 c, μ, L = 1, 1.426, 16
 N = Int(L) 
 ρ0 = N / L 
-
 @show get_mu(c, L, N)
 
 # ============ the BA solution ============ 
@@ -35,7 +34,7 @@ for qL in ph_actions
     end
 end
 
-msk_ba = scale_E.(energy.(states)) .< 4.5
+msk_ba = scale_E.(energy.(states)) .< 4.5; 
 states = states[msk_ba];
 
 momenta = momentum.(states)
@@ -45,9 +44,6 @@ function ρ0_form_factor(ψ1, ψ2, target)
     ln_norm_ρ, phase_ρ = ln_ρ0_form_factor(ψ1, ψ2; target=target, p=0.314159)
     return exp(ln_norm_ρ) * phase_ρ
 end
-
-#ρ0_ovlps_gs = [ρ0_form_factor(ψi, ψ0, :density) for ψi in states]
-#j0_ovlps_gs = [ρ0_form_factor(ψi, ψ0, :current) for ψi in states]
 
 chiral_ovlps_gs = [kacmoody(ψi, ψ0, :holomorphic, v, K) for ψi in states]
 antichiral_ovlps_gs = [kacmoody(ψi, ψ0, :antiholomorphic, v, K) for ψi in states]
@@ -85,7 +81,7 @@ Vs_cmps = []
 
 for k in -3:3
     global momenta_cmps, energies_cmps, numbers_cmps
-    @load "tmpdata1/excitation_c$(c)_mu$(μ)_L$(L)_k$(k)_chi$(χ).jld2" H1 N1 M1
+    @load "tmpdata/excitation_c$(c)_mu$(μ)_L$(L)_k$(k)_chi$(χ).jld2" H1 N1 M1
 
     H̃1 = sqrt(inv(N1)) * H1 * sqrt(inv(N1))
     M̃1 = sqrt(inv(N1)) * M1 * sqrt(inv(N1))
@@ -98,80 +94,69 @@ for k in -3:3
     energies_cmps = vcat(energies_cmps, Es[msk])
     momenta_cmps = vcat(momenta_cmps, fill(k*2*pi/L, length(Es[msk])))
 
-    push!(Es_cmps, Es)
-    push!(Vs_cmps, Vs)
+    push!(Es_cmps, Es[msk])
+    push!(Vs_cmps, inv(sqrt(N1)) * Vs[:, msk])
 end
 
 # apply Kac-Moody from ground state 
 v_cmps = (minimum(Es_cmps[4+1]) - Egs_cmps) / (2*pi/L)
 K_cmps = 2*pi*(Ngs_cmps/L)/ v_cmps
 
-numbers_cmps = ComplexF64[Ngs_cmps]
-chiral_ovlps_gs_cmps = ComplexF64[0]
-antichiral_ovlps_gs_cmps = ComplexF64[0]
-chiral_ovlps_ψ1_cmps = ComplexF64[]
-antichiral_ovlps_ψ1_cmps = ComplexF64[]
+chiral_ovlps_gs_cmps = Float64[0]
+antichiral_ovlps_gs_cmps = Float64[0]
+chiral_ovlps_ψ1_cmps = Float64[]
+antichiral_ovlps_ψ1_cmps = Float64[]
 
-V0 = Vs_cmps[4+1][:, 1]
-@load "tmpdata1/excitation_c$(c)_mu$(μ)_L$(L)_k1_chi$(χ).jld2" N1
-V0 = sqrt(inv(N1)) * V0
-
+### TODO. write a test . ovlpj01 ovlpj10 . j expectation = 2p 
 for k in -3:3
-    @load "tmpdata1/excitation_c$(c)_mu$(μ)_L$(L)_k$(k)_chi$(χ).jld2" H1 N1 M1
-    Vs = Vs_cmps[4+k]
-    Es = Es_cmps[4+k]
-    
-    M̃1 = sqrt(inv(N1)) * M1 * sqrt(inv(N1))
-    Ns = real.(diag(Vs' * M̃1 * Vs))
-    msk = (abs.(Ns .- Ngs_cmps) .< ΔN) .& (Es .- Egs_cmps .< ΔE * 5)
-
-    @show Ns[msk]
-    numbers_cmps = vcat(numbers_cmps, Ns[msk])
-
-    Δphase = 1 
-    for ix in Vector(1:χ^2)[msk]
+    for ix in eachindex(Es_cmps[4+k])
+        global chiral_ovlps_gs_cmps, antichiral_ovlps_gs_cmps, Vs_cmps
         global chiral_ovlps_ψ1_cmps, antichiral_ovlps_ψ1_cmps, V0
         
-        V1 = sqrt(inv(N1)) * Vs[:, ix]
+        V1 = Vs_cmps[4+k][:, ix]
 
         ovlpρ, ovlpj = Kac_Moody_gen(ψ, V1, k*2*pi/L, L, v_cmps, K_cmps, Ngs_cmps / L)
 
-        # Here the phase adjustment is based on the prior knowledge from the BA solution.
-        if ix == Vector(1:χ^2)[msk][1]
-            if abs(k) % 2 == 1
-                Δphase = norm(ovlpρ) / ovlpρ
-            else
-                Δphase = -norm(ovlpρ) / ovlpρ
-            end
-
-            if k == 1
-                pushfirst!(chiral_ovlps_ψ1_cmps, Δphase*(ovlpρ + ovlpj) / 2)
-                pushfirst!(antichiral_ovlps_ψ1_cmps, Δphase*(ovlpρ - ovlpj) / 2)
-            end
+        α = angle(ovlpρ)
+        if abs(k) == 2 && ix == 1 || abs(k) == 3 && ix == 2
+            # Here the phase adjustment is based on the prior knowledge from the BA solution,
+            # which is not necessary
+            α += pi 
         end
-        
-        #ovlpρ1, ovlpj1 = Kac_Moody_gen(ψ, V1, V0, k*2*pi/L, 2*pi/L, L, v_cmps, K_cmps, Ngs_cmps / L)
+        Vs_cmps[4+k][:, ix] *= exp(im*α)
+        @show α
 
-        ## Here the phase adjustment is based on the prior knowledge from the BA solution.
-        #if ix == Vector(1:χ^2)[msk][1]
-        #    if abs(k) % 2 == 1
-        #        Δphase1 = -norm(ovlpρ1) / ovlpρ1
-        #    else
-        #        Δphase1 = norm(ovlpρ1) / ovlpρ1
-        #    end
-        #end
+        push!(chiral_ovlps_gs_cmps, real((ovlpρ + ovlpj) / 2 / exp(im*α)))
+        push!(antichiral_ovlps_gs_cmps, real((ovlpρ - ovlpj) / 2 / exp(im*α)))
 
-        push!(chiral_ovlps_gs_cmps, Δphase*(ovlpρ + ovlpj) / 2)
-        push!(antichiral_ovlps_gs_cmps, Δphase*(ovlpρ - ovlpj) / 2)
-        @show k, ix, ovlpρ, ovlpj
-        #@show k, ix, ovlpρ1, ovlpj1
-        #push!(chiral_ovlps_ψ1_cmps, Δphase1*(ovlpρ1 + ovlpj1) / 2)
-        #push!(antichiral_ovlps_ψ1_cmps, Δphase1*(ovlpρ1 - ovlpj1) / 2)
+        if k == 1 && ix == 1
+            pushfirst!(chiral_ovlps_ψ1_cmps, real((ovlpρ + ovlpj) / 2 / exp(im*α)))
+            pushfirst!(antichiral_ovlps_ψ1_cmps, real((ovlpρ - ovlpj) / 2 / exp(im*α)))
+        end
+
+        @show k, ix, ovlpρ/exp(im*α), ovlpj/exp(im*α)
     end
 end
 
-@save "ovlps_cmps_c$(c)_mu$(μ)_L$(L).jld2" chiral_ovlps_gs_cmps antichiral_ovlps_gs_cmps chiral_ovlps_ψ1_cmps antichiral_ovlps_ψ1_cmps 
-@load "ovlps_cmps_c$(c)_mu$(μ)_L$(L).jld2" chiral_ovlps_gs_cmps antichiral_ovlps_gs_cmps chiral_ovlps_ψ1_cmps antichiral_ovlps_ψ1_cmps 
+# V for J_{-1} |gs>
+V0 = Vs_cmps[4+1][:, 1]
+for k in -3:3
+    for ix in eachindex(Es_cmps[4+k])
+        global chiral_ovlps_ψ1_cmps, antichiral_ovlps_ψ1_cmps, V0
+        
+        V1 = Vs_cmps[4+k][:, ix]
+
+        ovlpρ, ovlpj = Kac_Moody_gen(ψ, V1, V0, k*2*pi/L, 2*pi/L, L, v_cmps, K_cmps, Ngs_cmps / L)
+
+        push!(chiral_ovlps_ψ1_cmps, real((ovlpρ + ovlpj) / 2))
+        push!(antichiral_ovlps_ψ1_cmps, real((ovlpρ - ovlpj) / 2))
+
+        @show k, ix, ovlpρ, ovlpj
+    end
+end
+
+@save "tmpdata/ovlps_cmps_c$(c)_mu$(μ)_L$(L).jld2" chiral_ovlps_gs_cmps antichiral_ovlps_gs_cmps chiral_ovlps_ψ1_cmps antichiral_ovlps_ψ1_cmps 
+@load "tmpdata/ovlps_cmps_c$(c)_mu$(μ)_L$(L).jld2" chiral_ovlps_gs_cmps antichiral_ovlps_gs_cmps chiral_ovlps_ψ1_cmps antichiral_ovlps_ψ1_cmps 
 # ============ the plot ============ 
 
 CM = maximum(norm.(vcat(chiral_ovlps_gs, antichiral_ovlps_gs)))
@@ -189,16 +174,17 @@ end
 
 scale_E_cmps(x) = (x - Egs_cmps) / ΔE#(minimum(Es_cmps[4+1]) - Egs_cmps)
 
-function plot_spect_cmps(ax, ψi, ovlps)
-    msk = (scale_E_cmps.(energies_cmps) .< 1e-4) .& (abs.(momenta_cmps) .< 1e-4)
+function plot_spect_cmps(ax, exclusion, ovlps)
+    msk = (abs.(scale_E_cmps.(energies_cmps) .- exclusion[2]) .< 1e-2) .& (abs.(momenta_cmps .- exclusion[1]*2*pi/L) .< 1e-2)
     msk = .~(msk)
 
-    msk = msk .& (abs.(numbers_cmps .- Ngs_cmps) .< ΔN)
-    @show length(msk), length(momenta_cmps), length(energies_cmps), length(ovlps)
-
-    sc_main = scatter!(ax, momenta_cmps[msk] .* L ./ (2*pi), scale_E_cmps.(energies_cmps[msk]), color=real.(ovlps[msk]), colormap=mycmap, colorrange=(-CM, CM), marker='O', markersize=15, label="cMPS")
+    sc_main = scatter!(ax, momenta_cmps[msk] .* L ./ (2*pi), scale_E_cmps.(energies_cmps[msk]), color=ovlps[msk], colormap=mycmap, colorrange=(-CM, CM), marker='O', markersize=15, label="cMPS")
     return sc_main
 end
+
+θs = -pi/6:pi/30:pi/6
+horizon_xs = 5.8 * sin.(θs)
+horizon_ys = 2.7 * cos.(θs)
 
 font1 = Makie.to_font("/home/wtang/.local/share/fonts/STIXTwoText-Regular.otf")
 fig = Figure(backgroundcolor = :white, fontsize=18, resolution= (600, 500), fonts=(; regular=font1))
@@ -216,7 +202,8 @@ ylims!(ax1, (-0.15, 4.5))
 
 sc1_orig = scatter!(ax1, [0], [0], color=:darkgreen, marker=:star5, markersize=15)
 sc1_main = plot_spect(ax1, ψ0, chiral_ovlps_gs)
-sc1_main_cmps = plot_spect_cmps(ax1, ψ0, chiral_ovlps_gs_cmps)
+sc1_main_cmps = plot_spect_cmps(ax1, (0, 0), chiral_ovlps_gs_cmps)
+lines!(ax1, horizon_xs, horizon_ys, linestyle= :dash, color=:grey66)
 @show fig
 
 ax2 = Axis(gf[1, 2], 
@@ -228,39 +215,10 @@ ax2 = Axis(gf[1, 2],
 
 ylims!(ax2, (-0.15, 4.5))
 
-sc2_orig = scatter!(ax2, [0], [0], color=:darkgreen, marker=:star5, markersize=15)
-sc2_main = plot_spect(ax2, ψ0, antichiral_ovlps_gs)
-sc2_main_cmps = plot_spect_cmps(ax2, ψ0, antichiral_ovlps_gs_cmps)
-@show fig
-
-#ax3 = Axis(gf[2, 1], 
-#        xlabel = L"p L / 2\pi",
-#        #ylabel = L"\text{rescaled } \Delta E", 
-#        xticks = -4:1:4,
-#        yticks = 0:1:4,
-#        )
-#
-#ylims!(ax3, (-0.15, 4.5))
-#
-##sc3_orig = scatter!(ax3, [0], [0], color=:darkgreen, marker=:star5, markersize=15)
-#sc3_orig = scatter!(ax3, [1], [1], color=:darkgreen, marker=:star5, markersize=15)
-#sc3_main = plot_spect(ax3, ψ1, chiral_ovlps_ψ1)
-#sc3_main_cmps = plot_spect_cmps(ax3, ψ1, chiral_ovlps_ψ1_cmps)
-#@show fig
-#
-#ax4 = Axis(gf[2, 2], 
-#        xlabel = L"p L / 2\pi",
-#        #ylabel = L"\text{rescaled } \Delta E", 
-#        xticks = -4:1:4,
-#        yticks = 0:1:4,
-#        )
-#
-#ylims!(ax4, (-0.15, 4.5))
-#sc4_orig = scatter!(ax4, [1], [1], color=:darkgreen, marker=:star5, markersize=15)
-#sc4_main = plot_spect(ax4, ψ1, antichiral_ovlps_ψ1)
-#sc4_main_cmps = plot_spect_cmps(ax4, ψ1, antichiral_ovlps_ψ1_cmps)
-#@show fig
-
+sc2_orig = scatter!(ax2, [1], [1], color=:darkgreen, marker=:star5, markersize=15)
+sc2_main = plot_spect(ax2, ψ1, chiral_ovlps_ψ1)
+sc2_main_cmps = plot_spect_cmps(ax2, (1, 1), chiral_ovlps_ψ1_cmps)
+lines!(ax2, horizon_xs, horizon_ys, linestyle= :dash, color=:grey66)
 @show fig
 
 for (label, layout) in zip(["(a)", "(b)"], [gf[1, 1], gf[1, 2]])
@@ -284,10 +242,10 @@ axislegend(ax2, position=:lb, framevisible=true, labelsize=14)
 Colorbar(gl[1, 1], sc1_main, label=L"\text{form factor}", vertical = false, flipaxis = false) 
 
 @show fig
-Makie.save("fig-cmps-spect.pdf", fig)
+Makie.save("scripts/lieb-liniber-spectra/fig-cmps-spect.pdf", fig)
 
 
-
+# ============ export detailed data ============ 
 msk = (scale_E.(energies) .< 2.5) 
 msk_cmps = (scale_E_cmps.(energies_cmps) .< 2.5)
 
