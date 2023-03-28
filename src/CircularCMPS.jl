d70e2375f2a90b884381fea6b3db759a41de4d0f
Wei Tang <wei.tang.nju@gmail.com>
2023-03-28 15:50:53 +0200

    klein bottle entropy

--- src/CircularCMPS.jl
+++ src/CircularCMPS.jl
@@ -11,60 +11,27 @@ using Tullio
 using JLD2
 using Printf
 #using FastGaussQuadrature
-# Write your package code here.
 
 # utils.jl
-export MPSBondTensor,
-       GenericMPSTensor,
-       MPSTensor,
-       fill_data!,
-       randomize!,
-       K_permute,
-       K_permute_back,
-       K_otimes,
-       herm_reg_inv 
+export MPSBondTensor, GenericMPSTensor, MPSTensor, fill_data!, randomize!, K_permute, K_permute_back, K_otimes, herm_reg_inv 
 
 # cmps.jl
-export AbstractCMPSData, 
-       CMPSData, 
-       get_χ,
-       get_d, 
-       get_matrices, 
-       transfer_matrix, 
-       transfer_matrix_dagger, 
-       left_canonical, 
-       right_canonical, 
-       expand, 
-       K_mat, 
-       finite_env, 
-       rescale
+export AbstractCMPSData, CMPSData, get_χ, get_d, get_matrices, transfer_matrix, transfer_matrix_dagger, left_canonical, right_canonical, expand, K_mat, finite_env, rescale
 
 # operators.jl
-export kinetic, 
-       particle_density, 
-       point_interaction, 
-       pairing
+export kinetic, particle_density, point_interaction, pairing
 
 # ground_state.jl 
 export lieb_liniger_ground_state
 
 # excited_state.jl 
-export θ2, 
-       θ3, 
-       AbstractCoeffs,
-       Coeff2,
-       Coeff3, 
-       ExcitationData,
-       gauge_fixing_map,
-       effective_N, 
-       effective_H, 
-       Kac_Moody_gen
+export θ2, θ3, AbstractCoeffs, Coeff2, Coeff3, ExcitationData, gauge_fixing_map, effective_N, effective_H, Kac_Moody_gen
 
 # optim_alg.jl 
 export CircularCMPSRiemannian, minimize, leading_boundary_cmps
 
 # cmpo.jl 
-export AbstractCMPO, CMPO, ln_ovlp, compress, direct_sum, W_mul, variance, free_energy, energy
+export AbstractCMPO, CMPO, ln_ovlp, compress, direct_sum, W_mul, variance, free_energy, energy, klein
 
 # cmpo_zoo.jl
 export ising_cmpo, xxz_af_cmpo, heisenberg_j1j2_cmpo
