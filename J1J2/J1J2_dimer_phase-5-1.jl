5ed2705a6c8a5b05348e631cf20bc74bdf73315a
Wei Tang <wei.tang.nju@gmail.com>
2023-06-19 00:41:28 +0200

    struct PowerMethod

--- J1J2/J1J2_dimer_phase-5-1.jl
+++ J1J2/J1J2_dimer_phase-5-1.jl
@@ -12,9 +12,9 @@ T, Wmat = heisenberg_j1j2_cmpo(J1, J2)
 α = 2^(1/4)
 βs = 0.64 * α .^ (0:27)
 
-for β in βs
+for β in βs[19:end]
     @load "J1J2/cooling/dimer_phase_beta$(β).jld2" f E var ψ
-    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, 1e-8; maxiter=100, maxχ=20)
+    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, PowerMethod(tol_fidel=1e-8, tol_ES=1e-8, maxiter_compress=250))
     @save "J1J2/cooling/dimer_phase_beta-opt2$(β).jld2" β f E var ψ
     @show β, f, E, var
 end
