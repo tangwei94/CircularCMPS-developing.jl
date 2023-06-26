be3d153e4976ae887453b6dafcc768ca53a83432
Wei Tang <wei.tang.nju@gmail.com>
2023-06-26 12:52:38 +0200

    tol_fidel=1e-6

--- J1J2/J1J2_dimer_phase-5-1.jl
+++ J1J2/J1J2_dimer_phase-5-1.jl
@@ -7,14 +7,12 @@ using CircularCMPS
 J1, J2 = 1, 0.5
 T, Wmat = heisenberg_j1j2_cmpo(J1, J2)
 
-ψ = CMPSData(T.Q, T.Ls)
-
 α = 2^(1/4)
 βs = 0.64 * α .^ (0:27)
 
-for β in βs[19:end]
+for β in βs[23:end]
     @load "J1J2/cooling/dimer_phase_beta$(β).jld2" f E var ψ
-    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, PowerMethod(tol_fidel=1e-8, tol_ES=1e-8, maxiter_compress=250))
+    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, PowerMethod(tol_fidel=1e-6, tol_ES=1e-8, maxiter_compress=250))
     @save "J1J2/cooling/dimer_phase_beta-opt2$(β).jld2" β f E var ψ
     @show β, f, E, var
 end
--- heisenberg/heisenberg-2.jl
+++ heisenberg/heisenberg-2.jl
@@ -8,14 +8,12 @@ hz = 1
 hz = parse(Float64, ARGS[1])
 T, Wmat = xxz_af_cmpo(1; hz=hz)
 
-ψ = CMPSData(T.Q, T.Ls)
-
 α = 2^(1/4)
 βs = 0.32 * α .^ (0:32)
 
 for β in βs
     @load "heisenberg/results/cooling/heisenberg_hz$(hz)_beta$(β).jld2" ψ
-    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, PowerMethod(tol_fidel=1e-8, tol_ES=1e-8, maxiter_compress=200))
+    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, PowerMethod(tol_fidel=1e-6, tol_ES=1e-8, maxiter_compress=200))
     @save "heisenberg/results/cooling/heisenberg_hz$(hz)_beta$(β)-2.jld2" β f E var ψ
     @show β, f, E, var
 end
