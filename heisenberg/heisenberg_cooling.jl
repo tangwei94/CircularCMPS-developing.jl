59ad5263702264681d82403dcdb85e6c17472a98
Wei Tang <wei.tang.nju@gmail.com>
2023-07-18 11:55:51 +0200

    cooling instead of increase bond dimension

--- heisenberg/heisenberg_cooling.jl
+++ heisenberg/heisenberg_cooling.jl
@@ -15,7 +15,7 @@ T, Wmat = xxz_af_cmpo(1; hz=hz)
 
 for β in βs
     global ψ
-    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, PowerMethod(tol_ES=1e-7))
-    @save "heisenberg/results/cooling/heisenberg_hz$(hz)_beta$(β)-2.jld2" β f E var ψ
+    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, PowerMethod(tol_ES=1e-8))
+    @save "heisenberg/results/cooling/heisenberg_hz$(hz)_beta$(β)-toles8.jld2" β f E var ψ
     @show β, f, E, var
 end
