430eb3a06c4c014a64fdd51868f599e56e6a3b74
Wei Tang <wei.tang.nju@gmail.com>
2023-08-18 16:50:48 +0200

    update

--- ising/ising.jl
+++ ising/ising.jl
@@ -4,33 +4,28 @@ using JLD2
 using Revise
 using CircularCMPS
 
-T, W = ising_cmpo(1.0)
-ψ0 = CMPSData(T.Q, T.Ls)
+Γ = 1.0
+T, Wmat = ising_cmpo(Γ)
+ψ = CMPSData(T.Q, T.Ls)
 
-β = 10
-χ = 4
+α = 2^(1/4)
+βs = 0.32 * α .^ (0:32)
 
-fs = Float64[]
-ψs = CMPSData[]
-ss = Float64[]
-
-ψ1 = ψ0
-for χ in [4, 6, 8, 10]
-    for ix in 1:3 
-        Tψ1 = left_canonical(T*ψ1)[2]
-        ψ1 = compress(Tψ1, χ, β; tol=1e-6, init=ψ1)
-
-        f1 = free_energy(T, ψ1, ψ1, β)
-        s1 = klein(ψ1, ψ1, β)
-        @show ix, f1, s1
-    end
-    ψ1, f1, _, _, _ = leading_boundary_cmps(T, ψ1, β; maxiter=100)
-    s1 = klein(ψ1, ψ1, β)
-    @show χ, f1, s1
-    push!(fs, f1)
-    push!(ψs, ψ1)
-    push!(ss, s1)
+for β in βs
+    global ψ
+    ψ, f, E, var = power_iteration(T, Wmat, β, ψ, PowerMethod(tol_ES=1e-7))
+    @save "ising/results/ising_Gamma$(Γ)_beta$(β)-toles7.jld2" β f E var ψ
+    @show β, f, E, var
 end
 
-@show fs
-@show ss
+β = βs[30]
+
+@load "ising/results/ising_Gamma0.1_beta$(β)-toles7.jld2" ψ
+Λ, U = eigen(ψ.Q)
+Q1 = U' * ψ.Q * U
+R1 = U' * ψ.Rs[1] * U
+norm(R1[2]) - norm(R1[3])
+
+ψ1 = CMPSData(ψ.Q, -ψ.Rs)
+
+ln_ovlp(ψ1, ψ, β) + ln_ovlp(ψ, ψ1, β) - ln_ovlp(ψ, ψ, β) - ln_ovlp(ψ1, ψ1, β)
